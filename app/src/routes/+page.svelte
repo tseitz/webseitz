213ab7507a53fddbd085590e2ff68cb79fd5ecd8
tseitz <tdseitz10@gmail.com>
2023-02-14 11:27:14 -0600

    style: better formatting on work history

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -76,31 +76,52 @@
 	</blockquote>
 
 	<section class="col-start-2 col-span-10 bg-slate-800 rounded-lg p-8">
-		<h2>🌮 Work History</h2>
+		<h2 class="text-3xl">Work History 🧑🏼‍💻</h2>
 
-		<h3>McDonalds</h3>
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-			voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-			non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-		</p>
-		<h3>Burger King</h3>
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-			voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-			non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-		</p>
-		<h3>Taco Bell</h3>
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-			voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-			non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-		</p>
+		<h3 class="text-2xl mt-8 mb-2">ICON - Full Stack Developer (Apr 2018-Present)</h3>
+		<ul>
+			<ul class="list-disc list-outside">
+				<li>Began career learning Python and building an ETL layer for various projects</li>
+				<li>Kept up with the front-end world, self teaching React on the side.</li>
+				<li>Shifted to more of a front-end focus after ETL projects</li>
+				<li>Carried over Python knowledge to become a full stack engineer</li>
+				<li>
+					Worked with our architect to build out a back-end infrastructure with Azure, Python for
+					data processing and Flask APIs
+				</li>
+				<li>
+					Promoted to the role of Technical Lead for our Clinical Data Repository (CDR) project, a
+					project to centralize Clinical Data
+				</li>
+				<li>Used technical knowledge to build out a custom front-end stack from the ground up</li>
+				<li>
+					Shifted to more of a leader role, running our morning developer meetings and assisting
+					developers as necessary
+				</li>
+				<li>
+					Designed best practices and ways of working for the project, including project Git flow,
+					documentation and other administrative tasks
+				</li>
+				<li>
+					More than doubled testing speed by adjusting the testing stack mid way through phase 1 of
+					the project, greatly increasing developer productivity
+				</li>
+			</ul>
+
+			<h3 class="text-2xl mt-8 mb-2">Se2 - Software Developer (May 2014-Mar 2018)</h3>
+			<ul class="list-disc list-outside">
+				<li>
+					Sole developer of the interface’s team admin website using Angular (self taught 4+ years)
+				</li>
+				<li>Migrated system from Angular 1 to Angular 2</li>
+				<li>Moved to Web Development team in 2017 to continue working with Angular and C#</li>
+				<li>
+					Worked with senior developers to redesign the web interface with the goal of creating a
+					“Unified Web” to gather and show external clients their financial data.
+				</li>
+				<li>Team go-to for user experience design</li>
+				<li>Cross department recognition (web/interface work)</li>
+			</ul>
+		</ul>
 	</section>
 </main>
